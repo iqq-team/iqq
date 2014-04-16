60f66334f4ff0d37198a365efd77bd6777ab8f16
Zhihui_Chen <6208317@qq.com>
2014-04-16 19:34:23 +0800

    添加XmlUtils，读取/写入XML

--- main/src/main/java/iqq/app/core/service/SkinService.java
+++ main/src/main/java/iqq/app/core/service/SkinService.java
@@ -1,5 +1,9 @@
 package iqq.app.core.service;
 
+import com.alee.extended.painter.Painter;
+
+import java.awt.*;
+
 /**
  * Project  : iqq-projects
  * Author   : 承∮诺 < 6208317@qq.com >
@@ -8,4 +12,45 @@ package iqq.app.core.service;
  */
 public interface SkinService {
 
+    /**
+     * 获取颜色
+     *
+     * @return
+     */
+    public Color getColorByKey(String key);
+
+    /**
+     * 获取点9图的Painter
+     *
+     * @param key
+     * @return
+     */
+    public Painter getPainterByKey(String key);
+
+    /**
+     * 获取自定义皮肤目录
+     *
+     * @return
+     */
+    public String getDirectory();
+
+    /**
+     * 设置自定义皮肤目录
+     *
+     * @param path
+     */
+    public void setDirectory(String path);
+
+    /**
+     * 是否启用了自定义皮肤
+     * @return
+     */
+    public boolean isEnabledCustom();
+
+    /**
+     * 设置启用自定义皮肤
+     *
+     * @param enable
+     */
+    public void setEnableCustom(boolean enable);
 }
--- main/src/main/java/iqq/app/core/service/impl/SkinServiceImpl.java
+++ main/src/main/java/iqq/app/core/service/impl/SkinServiceImpl.java
@@ -1,9 +1,10 @@
-package iqq.app.core.service.impl;/**
- * Created by zhihui_chen on 14-4-15.
- */
+package iqq.app.core.service.impl;
 
+import com.alee.extended.painter.Painter;
 import iqq.app.core.service.SkinService;
 import org.nutz.ioc.loader.annotation.IocBean;
+
+import java.awt.*;
 
 /**
  * Project  : iqq-projects
@@ -13,4 +14,69 @@ import org.nutz.ioc.loader.annotation.IocBean;
  */
 @IocBean
 public class SkinServiceImpl implements SkinService {
+    public static final String SKIN_CONFIG_FILE = "skin.xml";
+    public static final String DEFAULT_SKIN_DIR = System.getProperty("app.dir",
+            System.getProperty("user.dir")) + "/skin-default/";
+
+    /**
+     * 获取颜色
+     *
+     * @param key
+     * @return
+     */
+    @Override
+    public Color getColorByKey(String key) {
+        return null;
+    }
+
+    /**
+     * 获取点9图的Painter
+     *
+     * @param key
+     * @return
+     */
+    @Override
+    public Painter getPainterByKey(String key) {
+        return null;
+    }
+
+    /**
+     * 获取自定义皮肤目录
+     *
+     * @return
+     */
+    @Override
+    public String getDirectory() {
+        return DEFAULT_SKIN_DIR;
+    }
+
+    /**
+     * 设置自定义皮肤目录
+     *
+     * @param path
+     */
+    @Override
+    public void setDirectory(String path) {
+
+    }
+
+    /**
+     * 是否启用了自定义皮肤
+     *
+     * @return
+     */
+    @Override
+    public boolean isEnabledCustom() {
+        return false;
+    }
+
+    /**
+     * 设置启用自定义皮肤
+     *
+     * @param enable
+     */
+    @Override
+    public void setEnableCustom(boolean enable) {
+
+    }
 }
